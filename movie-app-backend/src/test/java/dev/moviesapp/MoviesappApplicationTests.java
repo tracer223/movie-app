f7f0333b36e313681cfa5f0c38cb60f973fd39d9
Joshua Henderson <joshuahenderson031@gmail.com>
2024-03-18 14:29:09 -0700

    backend initializer code

---
+++ movie-app-backend/src/test/java/dev/moviesapp/MoviesappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.moviesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
