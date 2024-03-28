e0dace30327a158ec19a642e6c6c937267f1c5ff
Joshua Henderson <joshuahenderson031@gmail.com>
2024-03-28 14:45:23 -0700

    created apikey controller to retrieve api key from backend

---
+++ src/main/java/dev/moviesapp/movie/APIKeyController.java
@@ -0,0 +1,18 @@
+package dev.moviesapp.movie;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class APIKeyController {
+
+    @Value("${omdb.api.key}")
+    private String omdbApiKey;
+
+    @GetMapping("/api-key")
+    public ResponseEntity<String> getAPIKey() {
+        return ResponseEntity.ok(omdbApiKey);
+    }
+}
