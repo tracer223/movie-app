ca74e4deebe794f0b0dea37c84709c08a7b7d649
Joshua Henderson <joshuahenderson031@gmail.com>
2024-03-28 14:46:26 -0700

    searchResponse POJO

---
+++ src/main/java/dev/moviesapp/movie/SearchResponse.java
@@ -0,0 +1,20 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+public class SearchResponse {
+    @JsonProperty("Search")
+    private List<Movie> search;
+
+    public SearchResponse(List<Movie> search) {
+        this.search = search;
+    }
+
+    public List<Movie> getSearch() {
+        return search;
+    }
+
+    public void setSearch(List<Movie> search) {
+        this.search = search;
+    }
+
+}
